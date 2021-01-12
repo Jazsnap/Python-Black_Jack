4b99d8e4c907120036612adb3e843dc961ad172f
Jason Snape <54599218+Jazsnap@users.noreply.github.com>
2021-01-12 21:42:24 +0100

    Show_some and show_all methods added

--- BlackJack.py
+++ BlackJack.py
@@ -131,12 +131,16 @@ def hit_or_stand(deck,hand):
         break
 
 def show_some(player,dealer):
-    
-    pass
+    print("\nDealer's Hand:")
+    print(" <card hidden>")
+    print('',dealer.cards[1])  
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
     
 def show_all(player,dealer):
-    
-    pass
+    print("\nDealer's Hand:", *dealer.cards, sep='\n ')
+    print("Dealer's Hand =",dealer.value)
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+    print("Player's Hand =",player.value)
 
 def player_busts():
     pass
